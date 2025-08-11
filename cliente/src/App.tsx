0c21b2658d3f5db0af9050204682f5d2e03e3eb2
AndresDiaz1401 <anfedi140@gmail.com>
2025-08-11 18:56:17 -0500

    login terminado

--- cliente/src/App.tsx
+++ cliente/src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <><LoginPage/></>;
+  return (
+    <LoginPage />
+  );
 }
 
 export default App;
--- cliente/src/pages/LoginPage.tsx
+++ cliente/src/pages/LoginPage.tsx
@@ -1,28 +1,59 @@
 import "./LoginPage.css";
-import fotoLogin from "../assets/img/ticket.jpg";
+import fotoLogin from "../assets/img/ticket.png";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
+const CLIENTID = '616921257373-tu90riec4hfrc8aai06ibhch0cf1ao1v.apps.googleusercontent.com';
 
 function LoginPage() {
-    return ( <div className="loginPage">
-        <div className="loginPageIzquierda"> 
-        <h1>BOLETA<span className="verde">GO</span>
-        </h1>
-        <form>
+    return (
+        <GoogleOAuthProvider clientId={CLIENTID}>
+            <div>
+                <div className="loginPage">
+                    <div className="loginPageIzquierda">
+                        <h1>BOLETA<span className="verde">GO</span></h1>
+                        <form>
+                            <div className="botonGoogle">
+                                <GoogleLogin
+                                    onSuccess={credentialResponse => {
+                                        console.log("usuario logueado", credentialResponse);
+                                    }}
+                                    onError={() => {
+                                        console.log('Error de conexion');
+                                    }}
+                                    size="small"
+                                    theme="filled_black"
+                                    shape="pill"
+                                />
 
-        <input className="input correo" type="email" id="correo" name="correo" required placeholder="Correo Electronico"/>
-
-        <input className="input contraseña" type="password" id="contraseña" name="contraseña" required placeholder="Contraseña"/>
-        
-        <button className="botonLogin" type="submit">Iniciar sesion</button>
-        </form>
-        </div>
-        <div className="loginPageDerecha"> 
-            <img src={fotoLogin} alt="Fondo Boleta" />
-        </div>
-
-    </div>
-
+                            </div>
+                            <input
+                                className="input correo"
+                                type="email"
+                                id="correo"
+                                name="correo"
+                                required
+                                placeholder="Correo Electronico"
+                            />
+                            <input
+                                className="input contraseña"
+                                type="password"
+                                id="contraseña"
+                                name="contraseña"
+                                required
+                                placeholder="Contraseña"
+                            />
+                            <button className="botonLogin" type="submit">
+                                Iniciar sesion
+                            </button>
+                        </form>
+                    </div>
+                    <div className="loginPageDerecha">
+                        <img src={fotoLogin} alt="Fondo Boleta" />
+                    </div>
+                </div>
+            </div>
+        </GoogleOAuthProvider>
     );
-};
+}
 
 export default LoginPage;
