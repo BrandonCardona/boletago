73cebb07d69f29e5daa41925f6e5d5a8fa51eb21
AndresDiaz1401 <anfedi140@gmail.com>
2025-08-16 11:41:24 -0500

    registro

--- cliente/src/services/auth.ts
+++ cliente/src/services/auth.ts
@@ -1,7 +1,13 @@
 import type { LoginProps } from "../models/user";
+import type { RegisterProps } from "../models/user";
 import { rawAxios } from "./rawAxios.ts";
 
 export const loginUser = async ({ email, password }: LoginProps) => {
   const response = await rawAxios.post(`/auth/login`, { email, password });
+  return response.data;
+};
+
+export const registerUser = async ({nombre, email, password}: RegisterProps) =>{
+  const response = await rawAxios.post(`/auth/register`, { nombre, email, password });
   return response.data;
 };
