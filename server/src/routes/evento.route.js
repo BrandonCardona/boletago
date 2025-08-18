78608ba9a922cf08706c7905a4b302f639cfbf48
Brandon Cardona <cardonamartinezb@gmail.com>
2025-08-17 19:44:16 -0500

    Actualizacion backend

--- server/src/routes/evento.route.js
+++ server/src/routes/evento.route.js
@@ -4,29 +4,22 @@ import { requireAuth } from "../middlewares/requireAuth.js";
 import { hasRole } from "../middlewares/auth.js";
 import { DEFAULT_USERS } from "../libs/roles.js";
 
-
 export const eventoRouter = Router();
 
 eventoRouter.get("/eventos", WrappeEventosController.getEventos);
-eventoRouter.get(
-  "/:id",
-  [requireAuth, hasRole(DEFAULT_USERS.USER_ROLE)],
-  WrappeEventosController.getEventoById
-);
-eventoRouter.post(  
-    "/post",  
-    [requireAuth, hasRole(DEFAULT_USERS.USER_ROLE)],
+eventoRouter.get("/:id", [requireAuth], WrappeEventosController.getEventoById);
+eventoRouter.post(
+  "/post",
+  [requireAuth, hasRole()],
   WrappeEventosController.postEvento
 );
-eventoRouter.put(  
-    "/put/:id",  
-    [requireAuth, hasRole(DEFAULT_USERS.USER_ROLE)],
+eventoRouter.put(
+  "/put/:id",
+  [requireAuth, hasRole()],
   WrappeEventosController.putEvento
 );
-
 eventoRouter.delete(
   "/delete/:id",
-  [requireAuth, hasRole(DEFAULT_USERS.USER_ROLE)],
+  [requireAuth, hasRole()],
   WrappeEventosController.deleteEvento
-)
-
+);
